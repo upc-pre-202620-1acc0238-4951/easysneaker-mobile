4fdd59947e95cd24ff6e46997e514ceef120e7ff
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-03 10:20:10 -0500

    chore: add initial project structure and UI components for Easy Sneaker app

---
+++ app/src/main/java/pe/edu/upc/easysneaker/LoginScreen.kt
@@ -0,0 +1,119 @@
+package pe.edu.upc.easysneaker
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import pe.edu.upc.easysneaker.ui.theme.EasySneakerTheme
+import pe.edu.upc.easysneaker.ui.visibility
+import pe.edu.upc.easysneaker.ui.visibilityOff
+
+@Composable
+fun LoginScreen() {
+
+    val email = remember {
+        mutableStateOf("")
+    }
+
+    val password = remember {
+        mutableStateOf("")
+    }
+
+    val isPasswordHidden = remember {
+        mutableStateOf(true)
+    }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        OutlinedTextField(
+            value = email.value,
+            onValueChange = { newValue ->
+                email.value = newValue
+            },
+            placeholder = { Text(text = "Email") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.padding(vertical = 8.dp))
+
+        OutlinedTextField(
+            value = password.value,
+            onValueChange = { newValue ->
+                password.value = newValue
+            },
+            placeholder = { Text(text = "Password") },
+            visualTransformation = if (isPasswordHidden.value) {
+                PasswordVisualTransformation()
+            } else {
+                VisualTransformation.None
+            },
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        isPasswordHidden.value = !isPasswordHidden.value
+                    }
+                ) {
+                    Icon(
+                        imageVector = if (isPasswordHidden.value)
+                            visibilityOff else
+                            visibility, contentDescription = null
+                    )
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        )
+        Spacer(modifier = Modifier.padding(vertical = 8.dp))
+
+        Button(
+            onClick = {}, modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+            Text(
+                "Login"
+            )
+        }
+        OutlinedButton(
+            onClick = {}, modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+            Text(
+                "Register"
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    EasySneakerTheme {
+        LoginScreen()
+
+    }
+}
---
+++ app/src/main/java/pe/edu/upc/easysneaker/OnBoardingScreen.kt
@@ -0,0 +1,89 @@
+package pe.edu.upc.easysneaker
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import pe.edu.upc.easysneaker.ui.arrowForward
+import pe.edu.upc.easysneaker.ui.theme.EasySneakerTheme
+
+
+@Composable
+fun OnBoardingScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+        Image(
+            painter = painterResource(R.drawable.background),
+            contentDescription = "On Boarding Background"
+        )
+        Spacer(modifier = Modifier.weight(2f))
+        Text(
+            "Boost Your Style Sense",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            "Discover, shop, and unlock exciting sneakers tailored just for you",
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.weight(1f))
+
+        IconButton(
+            onClick = { },
+            colors = IconButtonDefaults.iconButtonColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer
+            )
+        ) {
+            Icon(
+                imageVector = arrowForward,
+                contentDescription = "Next",
+                modifier = Modifier
+                    .clip(
+                        CircleShape
+                    )
+
+
+            )
+
+        }
+
+
+
+        Spacer(modifier = Modifier.weight(1f))
+
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OnBoardingScreenPreview() {
+    EasySneakerTheme {
+        OnBoardingScreen()
+
+    }
+}
