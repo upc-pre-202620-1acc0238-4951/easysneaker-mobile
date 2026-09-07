5351f6ca0a23fbdaca35abbd0687832f555629be
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-07 15:37:18 -0500

    chore: update Gradle and AGP versions, remove unused imports

--- app/src/main/java/pe/edu/upc/easysneaker/features/home/presentation/ProductList.kt
+++ app/src/main/java/pe/edu/upc/easysneaker/features/home/presentation/ProductList.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
