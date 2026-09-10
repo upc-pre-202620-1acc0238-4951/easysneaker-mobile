14fa791e81465ccee515365ce1724131bae9079b
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-10 11:24:51 -0500

    feat(home): refactor ProductRepository to use Retrofit for network operations

--- app/src/main/java/pe/edu/upc/easysneaker/features/home/application/GetProductsUseCase.kt
+++ app/src/main/java/pe/edu/upc/easysneaker/features/home/application/GetProductsUseCase.kt
@@ -2,10 +2,10 @@ package pe.edu.upc.easysneaker.features.home.application
 
 import pe.edu.upc.easysneaker.features.home.domain.Product
 import pe.edu.upc.easysneaker.features.home.domain.ProductRepository
-import pe.edu.upc.easysneaker.features.home.infrastructure.InMemoryRepository
+import pe.edu.upc.easysneaker.features.home.infrastructure.repository.ProductRepositoryImpl
 
 class GetProductsUseCase(
-    private val productRepository: ProductRepository = InMemoryRepository()
+    private val productRepository: ProductRepository = ProductRepositoryImpl()
 ) {
     suspend operator fun invoke(): List<Product> = productRepository.getProducts()
 }
--- app/src/main/java/pe/edu/upc/easysneaker/features/home/infrastructure/repository/ProductRepositoryImpl.kt
+++ app/src/main/java/pe/edu/upc/easysneaker/features/home/infrastructure/repository/ProductRepositoryImpl.kt
@@ -3,8 +3,16 @@ package pe.edu.upc.easysneaker.features.home.infrastructure.repository
 import pe.edu.upc.easysneaker.features.home.domain.Product
 import pe.edu.upc.easysneaker.features.home.domain.ProductRepository
 import pe.edu.upc.easysneaker.features.home.infrastructure.remote.ProductService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class ProductRepositoryImpl(private val service: ProductService): ProductRepository {
+class ProductRepositoryImpl(
+    private val service: ProductService =
+        Retrofit.Builder().baseUrl("https://sneakerapi-y3ub.onrender.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build().create(ProductService::class.java)
+
+) : ProductRepository {
     override suspend fun getProducts(): List<Product> {
         val response = service.getProducts()
 
