6c0a91b11778b1ca0cda95afcfc35f68184d7e07
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-14 14:54:00 -0500

    feat(Home): add description field to products in InMemoryRepository

--- app/src/main/java/pe/edu/upc/easysneaker/features/home/infrastructure/repository/InMemoryRepository.kt
+++ app/src/main/java/pe/edu/upc/easysneaker/features/home/infrastructure/repository/InMemoryRepository.kt
@@ -5,7 +5,7 @@ import pe.edu.upc.easysneaker.features.home.domain.Product
 import pe.edu.upc.easysneaker.features.home.domain.ProductRepository
 import kotlin.time.Duration.Companion.milliseconds
 
-class InMemoryRepository: ProductRepository {
+class InMemoryRepository : ProductRepository {
 
     val products = listOf(
         Product(
@@ -13,23 +13,28 @@ class InMemoryRepository: ProductRepository {
             name = "Nike Air Motion",
             price = 122.64,
             rating = 4.9,
-            imageUrl = "https://i.imgur.com/E8ZBAnC.png"
+            imageUrl = "https://i.imgur.com/E8ZBAnC.png",
+            description = "Experience the ultimate in comfort and performance with the Nike Air Motion. Designed for runners, this shoe features responsive cushioning and a lightweight design that will keep you moving with ease."
         ),
         Product(
             id = 2,
             name = "ASICS Velocity Pro",
             price = 106.64,
             rating = 4.8,
-            imageUrl = "https://i.imgur.com/Uh1Jye7.png"
+            imageUrl = "https://i.imgur.com/Uh1Jye7.png",
+            description = "Experience the ultimate in comfort and performance with the Nike Air Motion. Designed for runners, this shoe features responsive cushioning and a lightweight design that will keep you moving with ease."
+
         ),
         Product(
             id = 3,
             name = "Adidas RunBoost X",
             price = 93.31,
             rating = 4.6,
-            imageUrl = "https://i.imgur.com/1yfHnqw.png"
+            imageUrl = "https://i.imgur.com/1yfHnqw.png",
+            description = "Experience the ultimate in comfort and performance with the Nike Air Motion. Designed for runners, this shoe features responsive cushioning and a lightweight design that will keep you moving with ease."
         )
     )
+
     override suspend fun getProducts(): List<Product> {
 
         delay(2000.milliseconds) // Simulate network delay
