2380dab4b7fbf3784f34e8d5f60faf484d59a4dc
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-17 10:36:02 -0500

    feat(AppNavHost): integrate BottomAppBar and improve layout with padding

--- app/src/main/java/pe/edu/upc/easysneaker/core/AppNavHost.kt
+++ app/src/main/java/pe/edu/upc/easysneaker/core/AppNavHost.kt
@@ -1,6 +1,10 @@
 package pe.edu.upc.easysneaker.core
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import pe.edu.upc.easysneaker.features.home.presentation.HomeRoute
@@ -8,11 +12,21 @@ import pe.edu.upc.easysneaker.features.home.presentation.homeNavGraph
 
 @Composable
 fun AppNavHost(navController: NavHostController) {
-    NavHost(
-        navController,
-        startDestination = HomeRoute
-    ) {
-        homeNavGraph(navController)
+    Scaffold(
+        bottomBar = {
+            BottomAppBar {
 
+            }
+        }
+    ) { paddingValues ->
+        NavHost(
+            navController,
+            startDestination = HomeRoute,
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            homeNavGraph(navController)
+
+        }
     }
+
 }
