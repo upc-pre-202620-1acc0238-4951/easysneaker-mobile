f849612633e872462033b16f2d90c6c08a725ad9
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-17 14:24:38 -0500

    refactor: update package structure for navigation components

--- app/src/main/java/pe/edu/upc/easysneaker/MainActivity.kt
+++ app/src/main/java/pe/edu/upc/easysneaker/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import pe.edu.upc.easysneaker.core.navigation.AppNavHost
+import pe.edu.upc.easysneaker.navigation.AppNavHost
 import pe.edu.upc.easysneaker.core.designsystem.theme.EasySneakerTheme
 
 @AndroidEntryPoint
--- app/src/main/java/pe/edu/upc/easysneaker/navigation/AppNavHost.kt
+++ app/src/main/java/pe/edu/upc/easysneaker/navigation/AppNavHost.kt
@@ -1,4 +1,4 @@
-package pe.edu.upc.easysneaker.core.navigation
+package pe.edu.upc.easysneaker.navigation
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
