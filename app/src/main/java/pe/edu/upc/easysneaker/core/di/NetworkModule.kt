b9effc6418b706fe99e87176ba9e78d65d9822e8
jmayta1984 <jmayta@pucp.edu.pe>
2026-09-17 14:20:22 -0500

    refactor: reorganize dependency injection modules for home feature

---
+++ app/src/main/java/pe/edu/upc/easysneaker/core/di/NetworkModule.kt
@@ -0,0 +1,32 @@
+package pe.edu.upc.easysneaker.core.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://sneakerapi-y3ub.onrender.com/api/")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
--- app/src/main/java/pe/edu/upc/easysneaker/features/home/infrastructure/di/HomeApiModule.kt
+++ app/src/main/java/pe/edu/upc/easysneaker/features/home/infrastructure/di/HomeApiModule.kt
@@ -6,21 +6,11 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import pe.edu.upc.easysneaker.features.home.infrastructure.remote.ProductService
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RemoteModule {
-
-    @Provides
-    @Singleton
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl("https://sneakerapi-y3ub.onrender.com/api/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
+object HomeApiModule {
 
     @Provides
     @Singleton
